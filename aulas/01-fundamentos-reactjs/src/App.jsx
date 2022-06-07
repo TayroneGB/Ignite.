82b25225d11ebfed247a827b92e9851fd94b3f22
Tayrone <tayrone.bez@gmail.com>
2022-06-07 18:27:35 -0300

    first commit

---
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,30 @@
+import { Header } from "./componentes/Header";
+import {Post} from "./Post";
+
+import styles from './App.module.css'
+
+import './global.css';
+import { Sidebar } from "./componentes/Sidebar";
+
+export function  App() {
+  return (
+    <div>
+      <Header/>
+
+      <div className={styles.wrapper}>
+      <Sidebar/>
+        <main>
+            <Post 
+              autor = "Diego "
+              content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, doloribus repellat fugit ullam voluptatibus facere nesciunt! Est odit perferendis obcaecati aliquam delectus? Velit delectus esse fugiat rem, soluta quibusdam consectetur!"
+          />
+          <Post 
+              autor = "Fernandes"
+              content ="legal"
+          />
+        </main>
+      </div>
+
+    </div>
+  )
+}
---
+++ aulas/01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,9 @@
+export function Post(props) {
+    return (
+    <div>
+        <strong>{props.autor}</strong>
+        <p>{props.content}</p>
+    </div>
+    )
+}
+
